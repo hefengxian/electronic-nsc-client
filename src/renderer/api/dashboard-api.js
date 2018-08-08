21fd9a1644c7effc92e67db7a65607a1e3cfb969
hfx <jssmith883@gmail.com>
2018-08-08 20:35:44 +0800

    增加 API 相关的实践

---
+++ src/renderer/api/dashboard-api.js
@@ -0,0 +1,13 @@
+/**
+ * Copyright © 2018-present, Knowlesys Ltd.
+ * All rights reserved.
+ *
+ * @author: HFX 2018-08-08 18-07
+ * @version 1.0
+ */
+
+import api from './api-client'
+
+export default {
+
+}
