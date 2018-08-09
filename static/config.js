32907f81ce87ff93d4fe372d8c9d5594642fa309
hfx <jssmith883@gmail.com>
2018-08-09 15:40:57 +0800

    增加配置文件

---
+++ static/config.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright © 2018-present, Knowlesys Ltd.
+ * All rights reserved.
+ *
+ * @author: HFX 2018-08-09 10-47
+ * @version 1.0
+ */
+
+// server端入口地址，例如（注意用 `/` 结尾）： http://192.168.1.xxx/server/
+window.SERVER_ROOT_URL = '/kwmcloud/server/'
+// API 地址前缀
+window.BASE_URL = window.SERVER_ROOT_URL + 'api'
